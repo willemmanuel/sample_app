7809311c8d1ab8fc80616b72d8f50cce893b6b70
willemmanuel <will.emmanuel@gmail.com>
2013-05-16 17:06:44 -0400

    Bootstrap working now

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,32 @@
 require 'spec_helper'
 
 describe "Static pages" do 
+	subject{ page }
 	describe "Home page" do
-		it "should have the h1 'Sample App'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => "Sample App")
-		end
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Ruby on Rails")
-		end
+		before {visit root_path}
+		it { should have_selector('h1', :text => "Shout") }
+		it { should have_selector('title', :text => "Ruby on Rails") }
 	end
-		describe "About page" do
-		it "should have the h1 '| About'" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', :text => "About")
-		end
-		it "should have the title 'About'" do
-			visit '/static_pages/about'
-			page.should have_selector('title', :text => "| About")
-		end
+	describe "About page" do
+		before {visit about_path}
+		it { should have_selector('h1', :text => "About") }
+		it { should have_selector('title', :text => "| About") }
 	end
-		describe "Help page" do
+	describe "Help page" do
+		before {visit help_path}
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
 			page.should have_selector('h1', :text => "Help")
 		end
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
 			page.should have_selector('title', :text => "| Help")
 		end
 	end
 	describe "Contact page" do
+		before {visit contact_path}
 		it "Should have the h1 'Contact" do
-			visit '/static_pages/contact'
 			page.should have_selector('h1', :text=>"Contact")
 		end
 		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
 			page.should have_selector('title', :text => "| Contact")
 		end
 	end
